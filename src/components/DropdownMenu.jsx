48c883f38e1b4789f8333b0971d44312ecc42809
Tan Phan <tanphan.dev@gmail.com>
2020-04-18 21:16:17 +0700

    feat: height transition

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ReactComponent as CogIcon } from '../icons/cog.svg';
 import { ReactComponent as ChevronIcon } from '../icons/chevron.svg';
 import { ReactComponent as ArrowIcon } from '../icons/arrow.svg';
+import { ReactComponent as BoltIcon } from '../icons/bolt.svg';
 import DropdownItem from './DropdownItem';
 import { CSSTransition } from 'react-transition-group';
 
 function DropdownMenu(props) {
   const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    console.log(dropdownRef.current.firstChild)
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
 
   return (
-    <div className="dropdown">
+    <div
+      className="dropdown"
+      style={{ height: menuHeight }}
+      ref={dropdownRef}
+      >
       <CSSTransition
         in={activeMenu === 'main'}
         unmountOnExit
         timeout={500}
         className="menu-primary"
+        onEnter={calcHeight}
       >
         <div className="menu">
-          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>My Profile</DropdownItem>
           <DropdownItem
             leftIcon={<CogIcon />}
             onClick={menu => setActiveMenu(menu)}
@@ -33,6 +51,7 @@ function DropdownMenu(props) {
         unmountOnExit
         timeout={500}
         className="menu-secondary"
+        onEnter={calcHeight}
       >
         <div className="menu">
         <DropdownItem
@@ -40,9 +59,10 @@ function DropdownMenu(props) {
             onClick={menu => setActiveMenu(menu)}
             goToMenu={'main'}
           >
-            Main
+            <h3>Settings</h3>
           </DropdownItem>
-          <DropdownItem>2</DropdownItem>
+          <DropdownItem leftIcon={<ChevronIcon />}>Privacy</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Account</DropdownItem>
         </div>
       </CSSTransition>
     </div>
